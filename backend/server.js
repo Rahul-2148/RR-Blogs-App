81322f37912bae141d71161b82fe9fa581f44468
RahulModi2148 <rahulrajmodi24523@gmail.com>
2024-10-22 01:24:31 +0530

    cors issue fix

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 4001;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
