f9564225a5a97a3e3d266d6c5dedb38f5a2a1c59
Rahul Raj <147687513+Rahul-2148@users.noreply.github.com>
2024-10-22 12:25:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 4001;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://rahulraj-blogs.vercel.app',  // Vercel frontend URL
+  origin: process.env.FRONTEND_URL, 'https://rahulraj-blogs.vercel.app',  // Vercel frontend URL added
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
